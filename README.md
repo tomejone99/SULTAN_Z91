57e1c142cdb54b899ff7bb7f59425bcca35a8c69
moody2020 <th3boss98@mail.ru>
2017-09-14 09:48:20 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# TH3BOSS
+# <p align="center" style="color:#cb3349" > [<< TH3BOSS >> V19 (Final Version)](https://telegram.me/llDEV1ll)
+
+ <p align="center" style="color: #14635c;" > بوت الزعيم الاقوى والاحدث لحمايه المجموعات في التلكرام
+<p align="center"><img src="زعيم.jpg" alt="بوت زعيم" title="بوت زعيم">
+
+***
+
+# <p align="center" style="color: #14635c;" > التنصيب بكود واحد
+```sh
+git clone https://github.com/moody2020/TH3BOSS.git && cd TH3BOSS && chmod +x TH3BOSS.sh && ./TH3BOSS.sh install && ./TH3BOSS.sh
+```
+
+
+<br>
+
+# <p align="center" style="color: #14635c;" >  تنصيب سورس الزعيم الاصدار 19 والاخير
+
+⇣ ┇  افتـح ترمنـــأل وخلي هاي
+```sh
+redis-server
+```
+⇣ ┇  اتركه مفتوح وافتح ترمنال ثاني وخلي
+```sh
+git clone https://github.com/moody2020/TH3BOSS.git
+```
+⇣ ┇  وراها هاي
+```sh
+cd TH3BOSS
+```
+⇣ ┇  وراها هاي 
+```sh
+chmod +x TH3BOSS.sh
+```
+⇣ ┇  وراها هاي ونتظر يكمل التنصيب 
+```sh
+./TH3BOSS.sh install
+```
+⇣ ┇  واخر شي هااي 
+```sh
+./TH3BOSS.sh
+```
+⇣ ┇للاستفسار راسلني 
+```sh
+@TH3BOSS
+```
+⇣ ┇للاستفسار راسلني 
+```sh
+@lBOSSl
+```
+##  [⇣ ┇انتظر قليلا ثم يطلب منك ايدي وتوكن خلي ايديك والتوكن مبروك عليك افضل بوت بالتلكرام](https://telegram.me/llDEV1ll)
+# <p align="center"> ⇣ ┇  للاستفسار راسلني 
+
+  [Mohammed Hisham](https://telegram.me/TH3BOSS) <br>
+  [TH3BOSS](https://telegram.me/LBOSSL) <br>
+  [CH - TH3BOSS](https://telegram.me/LLDEV1LL) 
---
+++ TH3BOSS.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+install() {
+		sudo apt-get update -y
+		sudo apt-get upgrade -y
+		sudo apt-get install lua5.1 lua-socket lua-sec redis-server curl -y
+		sudo apt-get install libreadline-dev libssl-dev lua5.2 luarocks liblua5.2-dev curl libcurl4-gnutls-dev -y
+		git clone http://github.com/keplerproject/luarocks
+		cd luarocks
+		./configure --lua-version=5.2
+		make build
+		sudo make install
+		sudo luarocks install Lua-cURL
+		sudo luarocks install oauth
+		sudo luarocks install redis-lua
+		sudo luarocks install lua-cjson
+		sudo luarocks install ansicolors
+		sudo luarocks install serpent
+		cd ..
+}
+
+function print_logo() {
+	green " "
+	green "                 ____ _____ _   _ _____ ____   ___  ____ ____  
+                 / __ \_   _| | | |___ /| __ ) / _ \/ ___/ ___| 
+                / / _  || | | |_| | |_ \|  _ \| | | \___ \___ \ 
+               | | (_| || | |  _  |___) | |_) | |_| |___) |__) |
+                \ \__,_||_| |_| |_|____/|____/ \___/|____/____/ 
+                 \____/ "
+	green " "
+	green " "
+	echo -e "\n\e[0m"
+}
+
+function logo_play() {
+    declare -A txtlogo
+    seconds="0.010"
+    txtlogo[1]=" "
+    txtlogo[2]="            ____ _____ _   _ _____ ____   ___  ____ ____  
+                 / __ \_   _| | | |___ /| __ ) / _ \/ ___/ ___| 
+                / / _  || | | |_| | |_ \|  _ \| | | \___ \___ \ 
+               | | (_| || | |  _  |___) | |_) | |_| |___) |__) |
+                \ \__,_||_| |_| |_|____/|____/ \___/|____/____/ 
+                 \____/ "
+    txtlogo[3]=" "
+    txtlogo[4]=" "
+    printf "\e[31m\t"
+    for i in ${!txtlogo[@]}; do
+        for x in `seq 0 ${#txtlogo[$i]}`; do
+            printf "${txtlogo[$i]:$x:1}"
+            sleep $seconds
+        done
+        printf "\n\t"
+    done
+    printf "\n"
+	echo -e "\e[0m"
+}
+
+function th3boss() {
+	echo -e "\e[0m"
+   	  red "     >>>>                       🌟┇ We Are Not Attacker                             "
+    green "     >>>>                       🌟┇ We Are Not Alliance                             "
+	white "     >>>>                       🌟┇ We Are Programmer                               "
+	white "     >>>>                       🌟┇ We Are The Best                                 "
+	  red "     >>>>                       🌟┇ We Are Family                                   "
+	  red "     >>>>                       🌟┇ @llDEV1ll                                       "
+	echo -e "\e[0m"
+}
+
+red() {
+  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
+}
+green() {
+  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
+}
+white() {
+  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
+}
+update() {
+	git pull
+}
+
+if [ "$1" = "install" ]; then
+	print_logo
+	th3boss
+	logo_play
+	install
+elif [ "$1" = "update" ]; then
+	logo_play
+	th3boss
+	update
+	exit 1
+else
+	print_logo
+	th3boss
+	logo_play
+	green " ⇣  جـاري تـشـغـيـل سـورس الـزعـيـم الاصـدار 19 ...."
+	lua ./bot/bot.lua
+fi
